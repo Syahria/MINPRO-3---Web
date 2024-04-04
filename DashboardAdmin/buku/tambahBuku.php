0819fc102feda90051eab5f07060b4dcc8d1d51c
Syahria <127468222+Syahria@users.noreply.github.com>
2024-04-04 20:19:26 +0800

    Update tambahBuku.php

--- DashboardAdmin/buku/tambahBuku.php
+++ DashboardAdmin/buku/tambahBuku.php
@@ -37,7 +37,7 @@ if(isset($_POST["tambah"]) ) {
 <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="../../assets/logoNav.png" alt="logo" width="120px">
+      <img src="../Librarylogo.png" alt="logo" width="120px">
         </a>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -120,13 +120,6 @@ if(isset($_POST["tambah"]) ) {
       </form>
     </div>
   </div>
-    
-  <footer class="mt-5 shadow-lg bg-subtle p-3">
-      <div class="container-fluid d-flex justify-content-between">
-      <p class="mt-2">Created by <span class="text-primary"> Mangandaralam Sakti</span> © 2023</p>
-      <p class="mt-2">versi 1.0</p>
-      </div>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
